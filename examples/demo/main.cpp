536378b08785a4ca8b140542b6b5478efbfba20f
tk-aria <tkaria.info@gmail.com>
2022-07-06 16:19:05 +0900

    fix demo code by clang-tidy

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -10,9 +10,6 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-const char* kMessage = "Hello World!";
 
 #include "void/common.h"
 
@@ -20,10 +17,6 @@ const char* kMessage = "Hello World!";
 
 int main(int argc, char** argv)
 {
-    char buf[128];
-    memcpy(buf, kMessage, sizeof(kMessage));
-    puts(buf);
-
     printf("[echo]: %s %s", echo_helloworld(), VOID_NEWLINE);
     return 0;
 }
