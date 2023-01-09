42b43664bb63b509994464313bfb924341db0a66
Kazuki Tanaka <tkaria.info@gmail.com>
2023-01-10 02:54:39 +0900

    Update makerunner.sh

--- bin/scripts/makerunner.sh
+++ bin/scripts/makerunner.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAKE_ARGS="-j$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g')+1))"
-make -j$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g')+1))
+#MAKE_ARGS="-j$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g')+1))"
+#make -j$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g')+1))
 
-#make -j6
+make -j$(nproc)
